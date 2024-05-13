bf458315879897e4999433cacfb380ffa2b51070
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-05-13 19:25:06 -0400

    feat: Add chats

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import Chats from './Chats'
+import Logo from './icons/Logo'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <div className='bg-red-500 flex justify-center'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </main>
+    <>
+      <header className='flex items-center justify-start gap-6 p-6 bg-gray-50 dark:bg-gray-950'>
+        <h1 className='text-2xl font-bold'>EPA Frontend</h1>
+        <Logo />
+      </header>
+      <main className='flex flex-col items-center justify-center p-6'>
+        <Chats />
+      </main>
+    </>
   )
 }
-
-export default App
