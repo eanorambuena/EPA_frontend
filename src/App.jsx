6036dae1198c0f57857f08961023d0d025198b71
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-05-14 16:02:18 +0000

    feat: Add login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Chats from './Chats'
+import Login from './Login'
 import Logo from './icons/Logo'
 
 export default function App() {
+  const vistas = {
+    1: <Chats />,
+    2: <Login />
+  }
   return (
     <>
-      <header className='flex items-center justify-start gap-6 p-6 bg-gray-50 dark:bg-gray-950'>
-        <h1 className='text-2xl font-bold'>EPA Frontend</h1>
+      <header className='flex items-center justify-start gap-6 p-6 bg-gray-50 dark:bg-gray-950 h-[150px] sm:h-[100px]'>
+        <h1 className='text-2xl font-bold'>EPA Chat</h1>
         <Logo />
       </header>
-      <main className='flex flex-col items-center justify-center p-6'>
-        <Chats />
+      <main className='flex flex-col items-center justify-center relative h-fit min-h-[calc(100dvh-150px)] sm:min-h-[calc(100dvh-100px)] p-6 bg-gray-100 dark:bg-gray-50'>
+        { vistas[2] }
       </main>
     </>
   )
