fc59a6f2dbb99a22d39f31448e6c273e050d5b27
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-05-14 19:25:01 +0000

    feat: Add chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Chat from './Chat'
 import Chats from './Chats'
 import Login from './Login'
 import Logo from './icons/Logo'
 
 export default function App() {
   const vistas = {
+    0: <Chat />,
     1: <Chats />,
     2: <Login />
   }
@@ -14,7 +16,7 @@ export default function App() {
         <Logo />
       </header>
       <main className='flex flex-col items-center justify-center relative h-fit min-h-[calc(100dvh-150px)] sm:min-h-[calc(100dvh-100px)] p-6 bg-gray-100 dark:bg-gray-50'>
-        { vistas[2] }
+        { vistas[0] }
       </main>
     </>
   )
--- src/ChatBubble.jsx
+++ src/ChatBubble.jsx
@@ -1,14 +1,18 @@
-export default ChatBubble() {
+export default function ChatBubble({ username, imgSrc, message, hourAndMinutes, me }) {
+  const color = me === true ? '#c4b5fd' : '#fcd34d'
   return (
-    <div className="flex items-start gap-2.5">
-      <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Jese image" />
-      <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-        <div className="flex items-center space-x-2 rtl:space-x-reverse">
-          <span className="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
-          <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
+    <div className={`flex gap-2.5 items:start ${me ? 'flex-row-reverse' : 'flex-row'}`}>
+      <img className="w-8 h-8 rounded-full" src={imgSrc} alt={username} />
+      <div
+        className={`flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-300 ${me ? 'rounded-s-xl rounded-ee-xl' : 'rounded-e-xl rounded-es-xl'}`}
+        style={{backgroundColor: color}}
+      >
+        <div className="flex items-center justify-between rtl:space-x-reverse">
+          <span className="text-sm font-semibold text-gray-900 dark:text-white">{username}</span>
+          <span className="text-sm font-normal text-gray-500 dark:text-gray-400">{hourAndMinutes}</span>
         </div>
-        <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">That's awesome. I think our users will really appreciate the improvements.</p>
-        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
+        <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{message}</p>
+        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">Recibido</span>
       </div>
       <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" data-dropdown-placement="bottom-start" className="inline-flex self-center items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-600" type="button">
         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
