8aca80b7dc3278d12ccba75d4b90d27d93be51b6
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-05-18 19:48:52 -0400

    feat: routing + main update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import Routing from './Routing'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>,
 )
