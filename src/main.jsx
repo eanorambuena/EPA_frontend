7b3b6f7bc270bd6a083aa193ab5a430bb32d537e
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-05-25 02:35:09 -0400

    feat: Add landing to page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Landing from './Landing'
 import './index.css'
 import Routing from './Routing'
 import { seed } from './services/seeder'
@@ -9,7 +8,6 @@ seed()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
     <Routing />
   </React.StrictMode>,
 )
