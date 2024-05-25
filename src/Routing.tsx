8f68985a7ce68be25b27629343e60800642a8562
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-05-25 03:26:22 -0400

    chore: Update package.json and add Landing component to Routing

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Chat from './Chat'
 import React from 'react'
 import MainPage from './MainPage'
+import Landing from './Landing'
 
 export default function Routing() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainPage />} />
+        <Route path='/' element={<Landing />} />
         <Route path="/about" />
         <Route path="/chats" element={<MainPage />} />
         <Route path='/chats/:id' element={<Chat />} />
