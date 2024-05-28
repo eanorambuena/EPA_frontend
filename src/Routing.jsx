fa70d7312644c29d652e139d3acdf7428e069c60
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-05-27 22:34:56 -0400

    fixed lint

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./Landing";
-import About from "./About";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './Landing'
+import About from './About'
 
 function Routing() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/about" element={<About />}/>
-                <Route path="/chats" />
-                <Route path="/docs" />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/chats" />
+        <Route path="/docs" />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default Routing;
+export default Routing
