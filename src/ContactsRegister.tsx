8840ece6e7db1470c6dd6a298acbec37e1dd758d
Pabloleiva00 <paleiva5@uc.cl>
2024-06-12 19:21:52 -0400

    fix: Run Eslint

--- src/ContactsRegister.tsx
+++ src/ContactsRegister.tsx
@@ -1,80 +1,87 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import SubmitButton from './components/SubmitButton';
-import Layout from './Layout';
+import React, { useState } from 'react'
+import axios from 'axios'
+import Layout from './Layout'
 
 interface Props {
-  searchParams?: { message: string };
+  searchParams?: { message: string }
 }
 
 // Definición de la función Login sin usar React.FC
 function ContactRegister({ searchParams }: Props) {
-  const [nickname, setNickname] = useState('');
-  const [userBase, setUserBase] = useState('');
-  const [userContact, setUserContact] = useState('');
+  const [nickname, setNickname] = useState('')
+  const [userBase, setUserBase] = useState('')
+  const [userContact, setUserContact] = useState('')
 
   const handleRegisterContact = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     console.log(userBase)
     console.log(userContact)
     console.log(nickname)
     const config_post = {
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       method: 'post',
-      url: `http://localhost:3000/contacts`,
+      url: 'http://localhost:3000/contacts',
       data: { 'nickname': nickname, 'userBase': userBase, 'userContact': userContact}
     }
 
     try {
-      const response_post = await axios(config_post);
-      console.log('Contacto registrado:', response_post.data);
+      const response_post = await axios(config_post)
+      console.log('Contacto registrado:', response_post.data)
     } catch (error) {
-      console.error('Error al registrar contacto:', error);
+      console.error('Error al registrar contacto:', error)
     }
-  };
+  }
 
   return (
     <Layout>
       <div className='flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 h-screen'>
         <form className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'>
-          <label className='text-md' htmlFor='phone'>
+          <label
+            className='text-md'
+            htmlFor='phone'
+          >
             Nickname
           </label>
           <input
-            type='Nickname'
             className='rounded-md px-4 py-2 bg-inherit border mb-6 border-violet-300'
             name='Nickname'
+            onChange={(e) => setNickname(e.target.value)}
             placeholder='Nickname'
             required
+            type='Nickname'
             value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
           />
-          <label className='text-md' htmlFor='password'>
+          <label
+            className='text-md'
+            htmlFor='password'
+          >
             User base
           </label>
           <input
-            type='userBase'
             className='rounded-md px-4 py-2 bg-inherit border mb-6 border-violet-300'
             name='userBase'
+            onChange={(e) => setUserBase(e.target.value)}
             placeholder='userBase'
             required
+            type='userBase'
             value={userBase}
-            onChange={(e) => setUserBase(e.target.value)}
           />
-
-          <label className='text-md' htmlFor='password'>
+          <label
+            className='text-md'
+            htmlFor='password'
+          >
             User Contact
           </label>
           <input
-            type='userContact'
             className='rounded-md px-4 py-2 bg-inherit border mb-6 border-violet-300'
             name='userContact'
+            onChange={(e) => setUserContact(e.target.value)}
             placeholder='userContact'
             required
+            type='userContact'
             value={userContact}
-            onChange={(e) => setUserContact(e.target.value)}
           />
           <button
             className='bg-amber-500 border border-foreground/20 rounded-md px-4 py-2 text-white mb-2'
@@ -90,7 +97,7 @@ function ContactRegister({ searchParams }: Props) {
         </form>
       </div>
     </Layout>
-  );
+  )
 }
 
-export default ContactRegister;
+export default ContactRegister
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,67 +1,75 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import SubmitButton from './components/SubmitButton';
-import Layout from './Layout';
+import React, { useState } from 'react'
+import axios from 'axios'
+import SubmitButton from './components/SubmitButton'
+import Layout from './Layout'
 
 interface Props {
-  searchParams?: { message: string };
+  searchParams?: { message: string }
 }
 
 // Definición de la función Login sin usar React.FC
 function Login({ searchParams }: Props) {
-  const [phone, setPhone] = useState('');
-  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleRegister = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     //console.log(phone)
     //console.log(password)
     const config_post = {
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       method: 'post',
-      url: `http://localhost:3000/users`,
+      url: 'http://localhost:3000/users',
       data: { 'phoneNumber': phone, 'password': password, 'type': 'user'}
     }
 
     try {
-      const response_post = await axios(config_post);
-      console.log('Usuario registrado:', response_post.data);
+      const response_post = await axios(config_post)
+      console.log('Usuario registrado:', response_post.data)
     } catch (error) {
-      console.error('Error al registrar el usuario:', error);
+      console.error('Error al registrar el usuario:', error)
     }
-  };
+  }
 
   return (
     <Layout>
       <div className='flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 h-screen'>
         <form className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'>
-          <label className='text-md' htmlFor='phone'>
+          <label
+            className='text-md'
+            htmlFor='phone'
+          >
             Teléfono
           </label>
           <input
-            type='tel'
             className='rounded-md px-4 py-2 bg-inherit border mb-6 border-violet-300'
             name='phone'
+            onChange={(e) => setPhone(e.target.value)}
             placeholder='+56912345678'
             required
+            type='tel'
             value={phone}
-            onChange={(e) => setPhone(e.target.value)}
           />
-          <label className='text-md' htmlFor='password'>
+          <label
+            className='text-md'
+            htmlFor='password'
+          >
             Contraseña
           </label>
           <input
-            type='password'
             className='rounded-md px-4 py-2 bg-inherit border mb-6 border-violet-300'
             name='password'
+            onChange={(e) => setPassword(e.target.value)}
             placeholder='••••••••'
             required
+            type='password'
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
           />
-          <SubmitButton className='mb-2'>Iniciar Sesión</SubmitButton>
+          <SubmitButton className='mb-2'>
+            Iniciar Sesión
+          </SubmitButton>
           <button
             className='bg-amber-500 border border-foreground/20 rounded-md px-4 py-2 text-white mb-2'
             onClick={handleRegister}
@@ -77,7 +85,7 @@ function Login({ searchParams }: Props) {
         </form>
       </div>
     </Layout>
-  );
+  )
 }
 
-export default Login;
+export default Login
