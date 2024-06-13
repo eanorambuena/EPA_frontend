a599626b0121095fff5b43b0ba03970bfcd079ce
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-13 01:12:56 -0400

    feat: Update landing page text and styles

--- src/Login.tsx
+++ src/Login.tsx
@@ -7,19 +7,30 @@ interface Props {
 }
 
 export default function Login({ searchParams } : Props) {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const phoneNumber = formData.get('phoneNumber') as string
+    const password = formData.get('password') as string
+    console.log({ phoneNumber, password })
+  }
+
   return (
     <Layout>
       <div className='flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 h-screen'>
-        <form className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'>
+        <form
+          className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground'
+          onSubmit={handleSubmit}
+        >
           <label
             className='text-md'
-            htmlFor='email'
+            htmlFor='phoneNumber'
           >
             Teléfono
           </label>
           <input
             className='rounded-md px-4 py-2 bg-inherit border mb-6 border-violet-300'
-            name='email'
+            name='phoneNumber'
             placeholder='+56912345678'
             required
           />
