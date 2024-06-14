75f377c1c15e2c0ad812f90fb79533cb337caca6
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-14 18:33:57 -0400

    feat: Update Header component to display user profile information
    The Header component has been updated to display the user's profile information. This change includes importing the useUserProfile hook and using it to fetch the user's profile based on their ID. The profile's username is now shown in the header, greeting the user with a personalized message. If the profile username is not available, a generic welcome message is displayed. This enhancement improves the user experience by providing personalized content in the header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,21 @@ import React, { useContext } from 'react'
 import Navbar from './/Navbar/navbar'
 import { useNavigate } from 'react-router-dom'
 import AuthContext from '../hooks/useCurrentUser'
+import useUserProfile from '../hooks/useUserProfile'
 
 export default function Header() {
   const navigate = useNavigate()
   const { user, logout } = useContext(AuthContext)
+  const profile = useUserProfile(user?.id)
 
   return (
     <header className='flex items-center justify-between gap-4 p-6 bg-gray-50 dark:bg-gray-950 h-fit max-w-full'>
       <Navbar />
       {user && (
         <section className='flex gap-4 items-center'>
-          Bienvenido!
-          {' '}
-          {user?.username || ''}
+          <span className='hidden sm:inline-block'>
+            {profile?.username ? `¡Bienvenido ${profile.username}!` : '¡Bienvenido de vuelta!'}
+          </span>
           <button
             className='bg-red-500 rounded-md shadow-sm px-4 py-2 text-white hover:scale-105 hover:bg-red-600 transition'
             onClick={logout}
