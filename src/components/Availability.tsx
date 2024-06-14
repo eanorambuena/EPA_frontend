83550d5e05a3ca3f64f0731eff424335298f5aba
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-14 03:50:02 -0400

    refactor: Remove unused code and improve Availability component

--- src/components/Availability.tsx
+++ src/components/Availability.tsx
@@ -1,45 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { Auth, ChatMemberSchema, ChatSchema, Status } from '../services/schema'
-import { useCurrentUser } from '../hooks/useCurrentUser'
-
-interface Props {
-  chat: ChatSchema
-}
+import React from 'react'
+import { Status } from '../services/schema'
 
 const colors = {
   [Status.online]: 'rgb(34, 197, 94)',
   [Status.offline]: 'rgb(239 68 68)'
 }
 
-export default function Availability({ chat } : Props) {
-  /*const user = useCurrentUser().user
-  const [otherUser, setOtherUser] = useState<Auth | null>(null)
-
-  useEffect(() => {
-    if (chat.isGroup)
-      return
-    const chatMembers: ChatMemberSchema[] = []
-    const otherChatMember = chatMembers.find((chatMember) => chatMember.userId !== user.id)
-    if (otherChatMember) {
-      //setOtherUser(otherChatMember.userId)
-    }
-  }, [chat, user])*/
-  return null
-  if (chat?.isGroup) return null
-
+export default function Availability() {
   return (
     <div className='flex items-center space-x-1'>
       <span
-        aria-label={`El usuario ${otherUser.name} está ${otherUser.status}`}
+        aria-label={`El usuario ${''} está ${Status.online}`}
         className='size-2 me-1 rounded-full'
-        style={{ backgroundColor: colors[otherUser.status] }}
+        style={{ backgroundColor: colors[Status.online] }}
       >
       </span>
       <span
         className='text-xs text-gray-500 dark:text-gray-400'
-        style={{ color: colors[otherUser.status] }}
+        style={{ color: colors[Status.online] }}
       >
-        {otherUser.status}
+        {Status.online}
       </span>
     </div>
   )
