563136a6433737014fe79f575ceb1886cb9770e4
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-14 21:12:18 -0400

    refactor: Remove unnecessary useEffect import in Chat.tsx

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Availability from './components/Availability'
 import Messages from './components/Messages'
 import SendMessageForm from './components/SendMessageForm'
