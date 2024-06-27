aedbef009234fd74411cfc1351b343599e77048f
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-06-27 15:36:45 -0400

    feat: progress with editing

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -4,6 +4,7 @@ import Messages from './components/Messages'
 import SendMessageForm from './components/SendMessageForm'
 import useChat from './hooks/useChat'
 import { useSelectedChatId } from './hooks/useSelectedChatId'
+import SubmitButton from './components/SubmitButton'
 
 interface Props {
   className?: string
@@ -23,6 +24,14 @@ export default function Chat({ className  }: Props) {
     )
   }
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    console.log('handleSubmit')
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const chatName = formData.get('chatName') as string
+    console.log(chatName)
+  }
+
   return (
     <div className={`h-full flex flex-col items-start justify-start ${className}`}>
       <header className='flex items-center justify-start w-full h-fit shadow-sm gap-6 p-4 bg-gray-50 dark:bg-gray-700 rounded-t-md'>
@@ -34,6 +43,21 @@ export default function Chat({ className  }: Props) {
         <h1 className='text-sm sm:text-md md:text-lg font-bold'>
           {chat.title}
         </h1>
+        <form 
+          className='flex-1 flex justify-end'
+          onSubmit={handleSubmit}  
+        >
+          <input className='rounded-md px-4 py-2 bg-inherit border border-violet-300'
+          placeholder='Cambiar nombre'
+          type='text'
+          id='chatName'
+          name='chatName'
+          required
+          />
+          <SubmitButton className='ml-2'>
+            Cambiar
+          </SubmitButton>
+        </form>
         <Availability />
       </header>
       <Messages messages={messages} />
