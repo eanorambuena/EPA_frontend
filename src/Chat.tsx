1cdef5460b1ddd7074e77a104ca51781d7c804de
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-06-27 20:28:36 -0400

    feat: edit chat name successfully

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,61 +1,59 @@
-import React from 'react'
-import Availability from './components/Availability'
-import Messages from './components/Messages'
-import SendMessageForm from './components/SendMessageForm'
-import useChat from './hooks/useChat'
-import { useSelectedChatId } from './hooks/useSelectedChatId'
-import SubmitButton from './components/SubmitButton'
-import axios from 'axios'
+import React from 'react';
+import Availability from './components/Availability';
+import Messages from './components/Messages';
+import SendMessageForm from './components/SendMessageForm';
+import useChat from './hooks/useChat';
+import { useSelectedChatId } from './hooks/useSelectedChatId';
+import SubmitButton from './components/SubmitButton';
+import axios from 'axios';
+import { API_URL } from './services/variables';
+import useAuthentication from './hooks/useAuthentication';
 
 interface Props {
-  className?: string
+  className?: string;
 }
 
-export default function Chat({ className  }: Props) {
-  const { selectedChatId } = useSelectedChatId()
-  const { chat, messages, appendMessage, image } = useChat(selectedChatId)
+export default function Chat({ className }: Props) {
+  const { selectedChatId } = useSelectedChatId();
+  const { chat, messages, appendMessage, image } = useChat(selectedChatId);
+  const authenticationConfig = useAuthentication(); // Move the hook call here
 
-  console.log('Chat', chat)
+  console.log('Chat', chat);
 
   if (!chat) {
     return (
       <div className={`h-full flex flex-col items-center justify-center ${className}`}>
-        <p className='text-lg font-bold'>
-          Selecciona un chat para comenzar
-        </p>
+        <p className='text-lg font-bold'>Selecciona un chat para comenzar</p>
       </div>
-    )
+    );
   }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const chatName = formData.get('chatName') as string
-    const chatId = chat.id
-    const response = await axios.patch(`http://localhost:3001/chats/${chatId}`, { title: chatName })
-    console.log(response)
-    if (response.status === 200) {
-      window.location.reload()
+    console.log('Submit');
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const chatName = formData.get('chatName') as string;
+    const chatId = chat.id;
+
+    try {
+      const response = await axios.patch(`${API_URL}/chats/${chatId}`, { title: chatName }, authenticationConfig);
+      console.log(response);
+      if (response.status === 200) {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error('Error updating chat:', error);
     }
-  }
+  };
 
   return (
     <div className={`h-full flex flex-col items-start justify-start ${className}`}>
       <header className='flex items-center justify-start w-full h-fit shadow-sm gap-6 p-4 bg-gray-50 dark:bg-gray-700 rounded-t-md'>
-        <img
-          alt={chat.title}
-          className='size-8 sm:size-10 rounded-full'
-          src={image}
-        />
-        <h1 className='text-sm sm:text-md md:text-lg font-bold'>
-          {chat.title}
-        </h1>
-        <form 
-          className='flex-1 flex justify-end'
-          onSubmit={handleSubmit}  
-        >
+        <img alt={chat.title} className='size-8 sm:size-10 rounded-full' src={image} />
+        <h1 className='text-sm sm:text-md md:text-lg font-bold'>{chat.title}</h1>
+        <form className='flex-1 flex justify-end' onSubmit={handleSubmit}>
           <label htmlFor='chatName' className='sr-only'>Cambiar nombre</label>
-          <input 
+          <input
             className='rounded-md px-4 py-2 bg-inherit border border-violet-300'
             placeholder='Cambiar nombre'
             type='text'
@@ -63,17 +61,12 @@ export default function Chat({ className  }: Props) {
             name='chatName'
             required
           />
-          <SubmitButton className='ml-2'>
-            Cambiar
-          </SubmitButton>
+          <SubmitButton className='ml-2'>Cambiar</SubmitButton>
         </form>
         <Availability />
       </header>
       <Messages messages={messages} />
-      <SendMessageForm
-        appendMessage={appendMessage}
-        chat={chat}
-      />
+      <SendMessageForm appendMessage={appendMessage} chat={chat} />
     </div>
-  )
+  );
 }
