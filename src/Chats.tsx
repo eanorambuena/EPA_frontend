09a2b1d8c2886e448d71ea53b08657fe514e567c
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-06-28 10:30:09 -0400

    Feat/create chats (#59)
    * feat: start of createChat page
    
    * feat: info is sent to backend

--- src/Chats.tsx
+++ src/Chats.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ChatRow from './components/ChatRow'
 import useChats from './hooks/useChats'
 import { useSelectedChatId } from './hooks/useSelectedChatId'
+import { API_URL } from './services/variables'
+import axios from 'axios'
+import { Auth } from './services/schema'
 
 interface Props {
   className?: string
@@ -11,6 +14,32 @@ export default function Chats({ className }: Props) {
   const { selectedChatId } = useSelectedChatId()
   const chats = useChats()
 
+  const createChat = async () => {
+    const user = await Auth.getCurrentUser()
+    if (!user || !Auth.currentAccessToken) {
+      console.error('User is not authenticated')
+      return
+    }
+
+    await axios.post(`${API_URL}/chats`, 
+      { title: 'Nuevo chat' },
+      {
+        headers: {
+          Authorization: `Bearer ${Auth.currentAccessToken}`
+        }
+      }
+    )
+    .then(async (response) => {
+      console.log('Chat creado:', response.data)
+      window.location.reload()
+      const chatId = response.data.id
+      window.location.href = `/chats/${chatId}`
+    })
+    .catch((error) => {
+      console.error('Error al crear chat:', error)
+    })
+  }
+
   return (
     <section
       className={`bg-gray-100 dark:bg-gray-800 rounded-md first:rounded-t-md last:rounded-b-md ${className}`}
@@ -18,7 +47,7 @@ export default function Chats({ className }: Props) {
     >
       <button
         className='w-full p-4 sm:p-6 font-4xl sm:font-3xl cursor-pointer rounded-md'
-        onClick={() => {}}
+        onClick={createChat}
         role='listitem'
       >
         Crear chat
