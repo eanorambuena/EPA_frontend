be4281c68c6ea82c18d29db8d4738b8bab3fbc78
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-06-28 20:03:12 -0400

    fix: lint

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -53,10 +53,6 @@ export default function Chat({ className  }: Props) {
     try {
       navigate('/chats')
       await axios.patch(`${API_URL}/chats/leave/${chat.id}`, {}, authentication)
-  const handleLeaveChat = async () => {
-    try {
-      navigate('/chats')
-      await axios.patch(`${API_URL}/chats/leave/${chat.id}`, {}, authentication)
     } catch (error) {
       console.error('Error al abandonar chat:', error)
     }
